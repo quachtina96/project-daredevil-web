99f90c1c6705e90f53f626165fcdf03773493994
Tina Quach <quacht@mit.edu>
2018-12-05 21:25:24 -0500

    successfully use webpack but chart not rendering

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: 'static',
+            to: '.'
+        }])
+    ],
+    watch: true
+};
