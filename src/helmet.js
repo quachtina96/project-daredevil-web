5a4b1501fea8de276d6c539c6628bb55f0651f83
Tina Quach <quacht@mit.edu>
2018-12-12 12:49:11 -0500

    expand helmet code

--- src/helmet.js
+++ src/helmet.js
@@ -14,8 +14,11 @@ state = {
 /**
  * @fileoverview This file defines the helmet controller.
  */
+
 class Helmet {
-	constructor {
+	constructor(channel) {
+		// The pheonix channel connected to the helmet
+		this.channel = channel;
 		// Current state of the helmet
 		this.state = {
 			'left': {
@@ -39,5 +42,25 @@ class Helmet {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Update internal state and send message to the microcontrollers on the
+	 * physical helmet
+	 * @param {!string} side - 'left' or 'right'. corresponds to which microcontroller
+	 * 	should respond to the message
+	 * @param {!Object} state - object denoting the speed and stop states of the
+	 * 	mounted motors
+	 */
+	update(side, state) {
+		this.state[side] = state;
+		channel.push(side, {body: JSON.stringify(state)})
+	}
+
+	// Stop the entire helmet from moving
+	stop(side) {
+		this.state[side].top.speed = 0;
+		this.state[side].bottom.speed = 0;
+		channel.push(side, {body: JSON.stringify(state)})
 	}
 }
